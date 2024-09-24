4f463953810cd07e8d425ab7ffbd1e25171f55eb
Adham Baskara <kk1258953@gmail.com>
2024-09-24 09:06:10 +0700

    PHP2_Prak8

--- Js5/fori.php
+++ Js5/fori.php
@@ -1,5 +1,13 @@
 <?php
-for ($i = 1; $i <= 25; $i++) {
-    echo "Perulangan ke-{$i} <br>";
+function tampilkanAngka(int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-{$indeks} <br>";
+
+    // Panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
 }
+
+// Memanggil fungsi dengan jumlah 20
+tampilkanAngka(20);
 ?>
