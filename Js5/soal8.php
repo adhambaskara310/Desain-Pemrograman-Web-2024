2f297fc99a345b98e666e888fe975bafbf911bae
Adham Baskara <kk1258953@gmail.com>
2024-09-26 02:55:43 +0700

    PHP2_Prak9

---
+++ Js5/soal8.php
@@ -0,0 +1,51 @@
+<?php
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
+                ]
+            ],
+            [
+                "nama" => "Kuliner"
+            ],
+            [
+                "nama" => "Hiburan"
+            ]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ]
+];
+
+function tampilkanMenuBertingkat(array $menu) {
+    echo "<ul>";
+    foreach ($menu as $item) {
+        echo "<li>{$item['nama']}</li>";
+
+        // Cek apakah item memiliki subMenu
+        if (isset($item['subMenu'])) {
+            // Panggil fungsi ini lagi untuk menampilkan subMenu
+            tampilkanMenuBertingkat($item['subMenu']);
+        }
+    }
+    echo "</ul>";
+}
+
+tampilkanMenuBertingkat($menu);
+?>
